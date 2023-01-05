ef1843b012313ccd5a5ee2891b01724575c0d486
Leandro Oliveira <leandro@agapsys.com>
2023-01-05 19:41:56 -0300

    Native implementation will be moved to a dedicated repo

--- src/main/c/hosts/linux/host.mk
+++
@@ -1,5 +0,0 @@
-ifeq ($(HOST),linux-x64)
-    ARTIFACT := jserial-jni-x64.so
-else
-    $(error Unsupported linux host: $(HOST))
-endif
--- src/main/c/hosts/windows/host.mk
+++
@@ -1,5 +0,0 @@
-ifeq ($(HOST),windows-x64)
-    ARTIFACT := jserial-jni-x64.dll
-else
-    $(error Unsupported windows host: $(HOST))
-endif
